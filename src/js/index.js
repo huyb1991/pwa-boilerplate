f2b00b5779de5581d66756fdbde00aa94451a7e0
Huy Nguyen <huyb.1991@gmail.com>
2020-04-25 12:23:41 +0700

    Add script to toggle Installer

--- src/js/index.js
+++ src/js/index.js
@@ -1,33 +1,64 @@
 const divInstall = document.getElementById('installContainer');
 const butInstall = document.getElementById('butInstall');
+const btnClose = document.getElementById('btnClose');
+const documentBody = document.getElementsByTagName('body')[0];
 
 window.addEventListener('beforeinstallprompt', (event) => {
   console.log('beforeinstallprompt', event);
   // Stash the event so it can be triggered later.
   window.deferredPrompt = event;
   // Remove the 'hidden' class from the install button container
-  divInstall.classList.toggle('hidden', false);
+  toggleInstall();
+  // divInstall.classList.toggle('hidden', false);
+  // documentBody.classList.toggle('app-with-installerbar', true)
 });
 
-butInstall.addEventListener('click', () => {
-  console.log('👍', 'butInstall-clicked');
-  const promptEvent = window.deferredPrompt;
-  if (!promptEvent) {
-    // The deferred prompt isn't available.
-    return;
+const toggleInstall = () => {
+  console.log('toggleInstall()')
+  // Installer container
+  if (divInstall.classList.contains('hidden')) {
+    console.log('container has')
+    divInstall.classList.remove('hidden');
+  } else {
+    console.log('container doesnt has')
+    divInstall.classList.add('hidden');
   }
-  // Show the install prompt.
-  promptEvent.prompt();
-  // Log the result
-  promptEvent.userChoice.then((result) => {
-    console.log('👍', 'userChoice', result);
-    // Reset the deferred prompt variable, since
-    // prompt() can only be called once.
-    window.deferredPrompt = null;
-    // Hide the install button.
-    divInstall.classList.toggle('hidden', true);
+
+  // Body
+  if (documentBody.classList.contains('app-with-installerbar')) {
+    documentBody.classList.remove('app-with-installerbar');
+  } else {
+    documentBody.classList.add('app-with-installerbar');
+  }
+}
+
+if (btnClose) {
+  btnClose.addEventListener('click', () => {
+    toggleInstall();
+  })
+}
+
+if (butInstall) {
+  butInstall.addEventListener('click', () => {
+    console.log('👍', 'butInstall-clicked');
+    const promptEvent = window.deferredPrompt;
+    if (!promptEvent) {
+      // The deferred prompt isn't available.
+      return;
+    }
+    // Show the install prompt.
+    promptEvent.prompt();
+    // Log the result
+    promptEvent.userChoice.then((result) => {
+      console.log('👍', 'userChoice', result);
+      // Reset the deferred prompt variable, since
+      // prompt() can only be called once.
+      window.deferredPrompt = null;
+      // Hide the install button.
+      divInstall.classList.toggle('hidden', true);
+    });
   });
-});
+}
 
 window.addEventListener('appinstalled', (event) => {
   console.log('👍', 'appinstalled', event);
